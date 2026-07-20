a6f3792eb59f5c560121733ce13ab3c66791899a
ANDRONIE DANUT <danutmg@gmail.com>
2026-07-20 08:47:02 +0300

    Update firebase-config.js

--- login/assets/js/firebase-config.js
+++ login/assets/js/firebase-config.js
@@ -17,18 +17,13 @@
   // https://firebase.google.com/docs/web/setup#available-libraries
 
   // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyCV66NlRO6XO5OdiQ7QoGceIyiRSzl5NxE",
-    authDomain: "fizica-galaction.firebaseapp.com",
-    projectId: "fizica-galaction",
-    storageBucket: "fizica-galaction.firebasestorage.app",
-    messagingSenderId: "705547484914",
-    appId: "1:705547484914:web:299e8afa54209764a9da6b",
-    measurementId: "G-K2SGZ47S6P"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+ // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCV66NlRO6XO5OdiQ7QoGceIyiRSzl5NxE",
+  authDomain: "fizica-galaction.firebaseapp.com",
+  projectId: "fizica-galaction",
+  storageBucket: "fizica-galaction.firebasestorage.app",
+  messagingSenderId: "705547484914",
+  appId: "1:705547484914:web:299e8afa54209764a9da6b",
+  measurementId: "G-K2SGZ47S6P"
+};

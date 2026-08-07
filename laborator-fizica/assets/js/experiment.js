b49441b58f61adeda44192e9568a33f53535a55d
ANDRONIE DANUT <danutmg@gmail.com>
2026-08-07 09:10:17 +0300

    Update experiment.js

--- laborator-fizica/assets/js/experiment.js
+++ laborator-fizica/assets/js/experiment.js
@@ -1,1 +1,20 @@
-
+[
+  {
+    "id": "energie-mecanica",
+    "title": "Conservarea energiei mecanice",
+    "domain": "Mecanică",
+    "class": "VII",
+    "icon": "assets/illustrations/energie.svg",
+    "tags": ["energie", "Ec", "Ep", "Em"],
+    "url": "mecanica/conservarea-energiei-mecanice/"
+  },
+  {
+    "id": "hooke",
+    "title": "Legea lui Hooke",
+    "domain": "Mecanică",
+    "class": "VII",
+    "icon": "assets/illustrations/hooke.svg",
+    "tags": ["elasticitate", "resort", "forță"],
+    "url": "mecanica/legea-lui-hooke/"
+  }
+]
